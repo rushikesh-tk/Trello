c0755d680abe2fc53b42f293dd47aea3bd60a62d
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2024-07-20 17:50:10 +0530

    Modal

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../components/Button";
 import SearchBar from "../components/SearchBar";
 import TodoChart from "../components/TodoChart";
+import Modal from "../components/Modal";
+import TaskModal from "./TaskModal";
 
 const Home = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="h-screen flex items-start justify-center bg-gray-600">
       <div className="text-3x w-11/12 h-5/6 mt-5">
@@ -14,14 +18,20 @@ const Home = () => {
             width: "10rem",
             height: "2rem",
             fontSize: "1rem",
-            marginBottom: "1rem",
+            marginBottom: "1.2rem",
             margin: "0.5rem",
           }}
+          onClick={() => setShowModal(true)}
         />
 
         <SearchBar />
-
         <TodoChart />
+
+        {showModal && (
+          <Modal onClose={setShowModal} title="Add Task">
+            <TaskModal />
+          </Modal>
+        )}
       </div>
     </div>
   );
