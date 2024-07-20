3291cc9b777260d3083b28b5adebba0d50d4107a
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2024-07-20 13:15:46 +0530

    Added initial routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/Navbar";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
 
 const App = () => {
-  return <div className="App">Heloooo</div>;
+  return (
+    <Router>
+      <div className="h-screen flex flex-col">
+        <NavBar />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
