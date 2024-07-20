f2a73afbc3deb7e4972bec18c981ba716eee8e6b
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2024-07-20 13:39:06 +0530

    navbar edited

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "./Button";
+import { GoGoal } from "react-icons/go";
 
 const NavBar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
-      <Link to="/" className="text-white justify-center">
-        Home
+    <nav className="bg-gray-800 p-4 flex items-center">
+      <Link to="/" className="text-white">
+        <GoGoal size={30} />
       </Link>
 
-      <Link to="/login" className="text-white float-right ml-5">
-        <Button text="Login" bgColor="blue" />
-      </Link>
+      <div className="ml-auto flex space-x-5">
+        <Link to="/login" className="text-white">
+          <Button text="Login" bgColor="blue" />
+        </Link>
 
-      <Link to="/signup" className="text-white float-right ml-5 ">
-        <Button text="Sign Up" bgColor="blue" />
-      </Link>
+        <Link to="/signup" className="text-white">
+          <Button text="Sign Up" bgColor="blue" />
+        </Link>
+      </div>
     </nav>
   );
 };
