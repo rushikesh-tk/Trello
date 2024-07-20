49b6ea0ea3b0cd36d0989a3537f630dd9ce6c6ea
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2024-07-20 17:51:18 +0530

    Search bar added

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,7 +1,27 @@
 import React from "react";
+import InputField from "./InputField";
 
 const SearchBar = () => {
-  return <div className="bg-pink-500 mb-2 m-2 rounded-lg">Search bar</div>;
+  return (
+    <div className="bg-white mb-2 m-2 rounded-md sm:flex  p-2 items-center sm:justify-between">
+      <div className="flex items-center justify-center">
+        <label
+          htmlFor="email"
+          className="block font-bold text-md text-gray-600 mr-2"
+        >
+          Search:
+        </label>
+        <input
+          type="text"
+          id="search"
+          className="p-1 block w-full border border-gray-500 rounded-md focus:outline-none"
+          placeholder="Search..."
+        />
+      </div>
+
+      <div>Sort By</div>
+    </div>
+  );
 };
 
 export default SearchBar;
