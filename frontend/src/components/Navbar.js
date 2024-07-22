42712360b1ed9890d54a489268ffb5055c82ca1d
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2024-07-22 10:49:42 +0530

    logout button handle

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "./Button";
 import { GoGoal } from "react-icons/go";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../actions/userActions";
+import toast from "react-hot-toast";
 
 const NavBar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const handleLogout = () => {
+    toast
+      .promise(dispatch(logout()), {
+        loading: "Logging out...",
+        success: "Logout successful!!",
+        error: "Network error",
+      })
+      .then(() => {
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.log("Error=>", err);
+      });
+  };
+
   return (
     <nav className="bg-gray-800 p-4 flex items-center">
       <Link to="/" className="text-white">
@@ -11,13 +35,27 @@ const NavBar = () => {
       </Link>
 
       <div className="ml-auto flex space-x-5">
-        <Link to="/login" className="text-white">
-          <Button text="Login" bgColor="blue" />
-        </Link>
+        {userInfo ? (
+          <div className="text-white">
+            <button
+              type="button"
+              className={`w-full flex items-center justify-center font-semibold bg-red-500 text-white p-2 rounded-md hover:bg-red-600`}
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </div>
+        ) : (
+          <>
+            <Link to="/login" className="text-white">
+              <Button text="Login" bgColor="blue" />
+            </Link>
 
-        <Link to="/signup" className="text-white">
-          <Button text="Sign Up" bgColor="blue" />
-        </Link>
+            <Link to="/signup" className="text-white">
+              <Button text="Sign Up" bgColor="blue" />
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
