8b2d0338e29f27bb245a71f729566bae9c09e233
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2024-07-22 10:50:30 +0530

    server created

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log("Heloo server.....");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+const PORT = 5000;
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/todos", todoRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
